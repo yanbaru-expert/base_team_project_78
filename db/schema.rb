79efb0311d3de5c68d7139c488f04395dc4f1bb0
k-guti14 <naka_kento_1207@icloud.com>
2021-10-04 18:45:41 +0900

    [メッセージ担当]new, create を実装（ストロングパラメータを利用）

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(version: 2021_10_03_150847) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.integer "age"
+    t.string "title"
+    t.integer "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
