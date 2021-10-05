10b6b919b4cfc809e999b2b9b60bf4f4072eb730
k-guti14 <naka_kento_1207@icloud.com>
2021-10-05 16:01:49 +0900

    [メッセージ担当]edit, update を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,23 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    User.create(user_params)
+  end
+
   def show
     @user = User.find(params[:id])
   end
 
-  def create
-    User.create(user_params)
+  def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+  end
+  
 
   private
   def user_params
