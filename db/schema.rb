6e610d027919a8b0b57b45d7fc077edc5541980d
k-guti14 <naka_kento_1207@icloud.com>
2021-10-05 20:01:47 +0900

    [メッセージ担当]コントローラを作成し，ルーティングをresourcesで定義

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2021_10_02_224514) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "tasks", force: :cascade do |t|
+  create_table "meaasages", force: :cascade do |t|
     t.string "title"
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
