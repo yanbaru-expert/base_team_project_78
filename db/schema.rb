ce563d1c97ca471b9b0d5479134e185cae6938c5
k-guti14 <naka_kento_1207@icloud.com>
2021-10-05 20:22:14 +0900

    [メッセージ担当][修正]rails db:migrateを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_02_224514) do
+ActiveRecord::Schema.define(version: 2021_10_05_104327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "meaasages", force: :cascade do |t|
+  create_table "messages", force: :cascade do |t|
     t.string "title"
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
