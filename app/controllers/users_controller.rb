ef7587a16db788a089291ada47404e192117cf44
R.moriyama <moriforest537@gmail.com>
2021-10-11 22:58:29 +0900

    wip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,28 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
+  end
+
+  def create
+    User.create(user_params)
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :age)
   end
 end
